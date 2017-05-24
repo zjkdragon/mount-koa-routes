262a7938093b98a95afdc4f24024ea040f94dc86
kai <zjkdragon@163.com>
2017-05-24 14:22:58 +0800

    Update index.js
    use convert wrap koa-router

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var requireDirectory = require('require-directory');
 var routes = requireDirectory(module, './routes');
 
 var koa = require('koa-router')(); 
+var convert = require('koa-convert');
 
 var stack = [];
 /**
@@ -147,7 +148,7 @@ function mount_with_folder(app, routes_folder_path) {
   
   mount(koa) ;
   // console.log(koa.routes());
-  app.use(koa.routes());
+  app.use(convert(koa.routes()));
   
   if(is_debug){
     _dump (routes_folder_path);
